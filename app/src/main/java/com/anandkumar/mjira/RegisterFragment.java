1cd21ff1ba3bfb52f93d06573b6b84e43da6f792
karanianandkumar <karanianandkumar@yahoo.com>
2016-06-13 00:47:01 +0530

    21. Updating RegisterFragment with Floating TextViews and TextWatchers.

--- app/src/main/java/com/anandkumar/mjira/RegisterFragment.java
+++ app/src/main/java/com/anandkumar/mjira/RegisterFragment.java
@@ -3,10 +3,15 @@ package com.anandkumar.mjira;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -29,6 +34,12 @@ public class RegisterFragment extends Fragment {
     private EditText mailField;
     private String deviceID;
 
+    private Preferences saveData;
+
+    private EditText inputName, inputEmail, inputPassword;
+    private TextInputLayout inputLayoutName, inputLayoutEmail, inputLayoutPassword;
+    private Button btnSignUp;
+
     public RegisterFragment() {
         // Required empty public constructor
     }
@@ -40,65 +51,31 @@ public class RegisterFragment extends Fragment {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_register, container, false);
 
-        usernameField=(EditText)view.findViewById(R.id.userNameField);
-        passwordField=(EditText)view.findViewById(R.id.passwordField);
-        mailField=(EditText)view.findViewById(R.id.loginMailField);
-        final Preferences saveData=new Preferences();
+        inputLayoutName = (TextInputLayout) view.findViewById(R.id.input_layout_name);
+        inputLayoutEmail = (TextInputLayout) view.findViewById(R.id.input_layout_email);
+        inputLayoutPassword = (TextInputLayout) view.findViewById(R.id.input_layout_password);
+        inputName = (EditText) view.findViewById(R.id.input_name);
+        inputEmail = (EditText) view.findViewById(R.id.input_email);
+        inputPassword = (EditText) view.findViewById(R.id.input_password);
+        btnSignUp = (Button) view.findViewById(R.id.btn_signup);
+
+        inputName.addTextChangedListener(new MyTextWatcher(inputName));
+        inputEmail.addTextChangedListener(new MyTextWatcher(inputEmail));
+        inputPassword.addTextChangedListener(new MyTextWatcher(inputPassword));
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                submitForm();
+            }
+        });
+
+
 
         String deviceId_pref=saveData.readString(getActivity().getApplicationContext(),saveData.DEVICE_ID,null);
 
         Toast.makeText(getActivity(), "The register Id  is(4):\t" +deviceId_pref , Toast.LENGTH_SHORT).show();
 
-        registerButton=(Button)view.findViewById(R.id.register);
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String username=usernameField.getText().toString();
-                String password=passwordField.getText().toString();
-                String mail=mailField.getText().toString();
-
-                BackendlessUser backendlessUser=new BackendlessUser();
-                backendlessUser.setPassword(password);
-                backendlessUser.setProperty("name",username);
-                backendlessUser.setEmail(mail);
-
-                backendlessUser.setProperty("device",saveData.readString(getActivity().getApplicationContext(),saveData.DEVICE_ID,null));
-
-                Toast.makeText(getActivity(), "The register Id  is(5):\t" + saveData.readString(getActivity().getApplicationContext(),saveData.DEVICE_ID,null), Toast.LENGTH_SHORT).show();
-
-
-
-
-
-
-                Backendless.UserService.register(backendlessUser, new AsyncCallback<BackendlessUser>() {
-                    @Override
-                    public void handleResponse(BackendlessUser response) {
-
-                        /*SaveData saveData1=new SaveData(getActivity().getApplicationContext());
-                        saveData1.setCurrentUser(response.getProperty("name").toString());
-                        saveData1.setDeviceId(response.getProperty("device").toString());
-                        Toast.makeText(getActivity(),"Successfully Register",Toast.LENGTH_SHORT).show();
-
-
-
-                        Toast.makeText(getActivity(),"User Name is :\t "+saveData.getCurrentUser()+" device ID is: \t "+saveData.getDeviceId(),Toast.LENGTH_SHORT).show();
-
-                        Toast.makeText(getActivity(),"User Name is :\t "+saveData1.getCurrentUser()+" device ID is: \t "+saveData1.getDeviceId(),Toast.LENGTH_SHORT).show();*/
-                        Toast.makeText(getActivity(),"Successfully Registered...",Toast.LENGTH_SHORT).show();
-
-                        Intent intent=new Intent(getActivity(),MainActivity.class);
-                        startActivity(intent);
-                    }
-
-                    @Override
-                    public void handleFault(BackendlessFault fault) {
-                        Toast.makeText(getActivity(),"User not Registered",Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
 
 
 
@@ -107,4 +84,131 @@ public class RegisterFragment extends Fragment {
         return view;
     }
 
+
+    /**
+     * Validating form
+     */
+    private void submitForm() {
+        if (!validateName()) {
+            return;
+        }
+
+        if (!validateEmail()) {
+            return;
+        }
+
+        if (!validatePassword()) {
+            return;
+        }
+
+
+        String username=inputName.getText().toString();
+        String password=inputPassword.getText().toString();
+        String mail=inputEmail.getText().toString();
+
+        BackendlessUser backendlessUser=new BackendlessUser();
+        backendlessUser.setPassword(password);
+        backendlessUser.setProperty("name",username);
+        backendlessUser.setEmail(mail);
+
+        backendlessUser.setProperty("device",saveData.readString(getActivity().getApplicationContext(),saveData.DEVICE_ID,null));
+
+
+
+        Backendless.UserService.register(backendlessUser, new AsyncCallback<BackendlessUser>() {
+            @Override
+            public void handleResponse(BackendlessUser response) {
+
+
+                Toast.makeText(getActivity(),"Successfully Registered...",Toast.LENGTH_SHORT).show();
+
+                Intent intent=new Intent(getActivity(),MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+                Toast.makeText(getActivity(),"User not Registered",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private boolean validateName() {
+        if (inputName.getText().toString().trim().isEmpty()) {
+            inputLayoutName.setError(getString(R.string.err_msg_name));
+            requestFocus(inputName);
+            return false;
+        } else {
+            inputLayoutName.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validateEmail() {
+        String email = inputEmail.getText().toString().trim();
+
+        if (email.isEmpty() || !isValidEmail(email)) {
+            inputLayoutEmail.setError(getString(R.string.err_msg_email));
+            requestFocus(inputEmail);
+            return false;
+        } else {
+            inputLayoutEmail.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validatePassword() {
+        if (inputPassword.getText().toString().trim().isEmpty()) {
+            inputLayoutPassword.setError(getString(R.string.err_msg_password));
+            requestFocus(inputPassword);
+            return false;
+        } else {
+            inputLayoutPassword.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private static boolean isValidEmail(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    private class MyTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private MyTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.input_name:
+                    validateName();
+                    break;
+                case R.id.input_email:
+                    validateEmail();
+                    break;
+                case R.id.input_password:
+                    validatePassword();
+                    break;
+            }
+        }
+    }
 }
+
+
