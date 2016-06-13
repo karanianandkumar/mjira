6bcfcedae710b43ea768ad1159e5145a489b22e0
karanianandkumar <karanianandkumar@yahoo.com>
2016-06-13 08:10:40 +0530

    22.Login UI UPdated

--- app/src/main/java/com/anandkumar/mjira/LoginFragment.java
+++ app/src/main/java/com/anandkumar/mjira/LoginFragment.java
@@ -3,10 +3,15 @@ package com.anandkumar.mjira;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -22,9 +27,11 @@ import com.backendless.exceptions.BackendlessFault;
  */
 public class LoginFragment extends Fragment {
 
-    private Button loginButton;
-    private EditText userNameField;
-    private EditText passwordField;
+
+
+    private EditText inputName, inputPassword;
+    private TextInputLayout inputLayoutName, inputLayoutPassword;
+    private Button btnSignIn;
 
 
     public LoginFragment() {
@@ -38,47 +45,141 @@ public class LoginFragment extends Fragment {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_login, container, false);
 
-        userNameField=(EditText)view.findViewById(R.id.loginUserNameField);
-        passwordField=(EditText)view.findViewById(R.id.loginPasswordField);
 
-        loginButton=(Button)view.findViewById(R.id.login);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        inputLayoutName = (TextInputLayout) view.findViewById(R.id.input_layout_name);
+
+        inputLayoutPassword = (TextInputLayout) view.findViewById(R.id.input_layout_password);
+        inputName = (EditText) view.findViewById(R.id.input_name);
+
+        inputPassword = (EditText) view.findViewById(R.id.input_password);
+        btnSignIn = (Button) view.findViewById(R.id.btn_signin);
+
+        inputName.addTextChangedListener(new MyTextWatcher(inputName));
+
+        inputPassword.addTextChangedListener(new MyTextWatcher(inputPassword));
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                final String userName=userNameField.getText().toString();
-                String password=passwordField.getText().toString();
-
-
-                Backendless.UserService.login(userName, password, new AsyncCallback<BackendlessUser>() {
-                    @Override
-                    public void handleResponse(BackendlessUser response) {
-                        Toast.makeText(getActivity(),
-                                "Succefully Loged in",
-                                Toast.LENGTH_SHORT
-                        ).show();
-
-                        Preferences saveData=new Preferences();
-                        saveData.writeString(getActivity().getApplicationContext(), saveData.USER_NAME, userName);
-
-                       Intent intent=new Intent(getActivity(),MainActivity.class);
-                       startActivity(intent);
-                    }
-
-                    @Override
-                    public void handleFault(BackendlessFault fault) {
-
-                        Toast.makeText(getActivity(),
-                                "Failed to Loged in",
-                                Toast.LENGTH_SHORT
-                        ).show();
-                    }
-                },true);
+            public void onClick(View view) {
+                submitForm();
             }
         });
+
+
+
 
 
         return view;
     }
 
+    /**
+     * Validating form
+     */
+    private void submitForm() {
+        if (!validateName()) {
+            return;
+        }
+
+
+
+        if (!validatePassword()) {
+            return;
+        }
+
+
+        final String userName=inputName.getText().toString();
+        String password=inputPassword.getText().toString();
+
+
+
+
+        Backendless.UserService.login(userName, password, new AsyncCallback<BackendlessUser>() {
+            @Override
+            public void handleResponse(BackendlessUser response) {
+                Toast.makeText(getActivity(),
+                        "Succefully Loged in",
+                        Toast.LENGTH_SHORT
+                ).show();
+
+                Preferences saveData=new Preferences();
+                saveData.writeString(getActivity().getApplicationContext(), saveData.USER_NAME, userName);
+
+                Intent intent=new Intent(getActivity(),MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void handleFault(BackendlessFault fault) {
+
+                Toast.makeText(getActivity(),
+                        "Failed to Loged in",
+                        Toast.LENGTH_SHORT
+                ).show();
+            }
+        },true);
+
+    }
+
+    private boolean validateName() {
+        if (inputName.getText().toString().trim().isEmpty()) {
+            inputLayoutName.setError(getString(R.string.err_msg_name));
+            requestFocus(inputName);
+            return false;
+        } else {
+            inputLayoutName.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+
+
+    private boolean validatePassword() {
+        if (inputPassword.getText().toString().trim().isEmpty()) {
+            inputLayoutPassword.setError(getString(R.string.err_msg_password));
+            requestFocus(inputPassword);
+            return false;
+        } else {
+            inputLayoutPassword.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private static boolean isValidEmail(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    private class MyTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private MyTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.input_name:
+                    validateName();
+                    break;
+
+                case R.id.input_password:
+                    validatePassword();
+                    break;
+            }
+        }
+    }
 }
