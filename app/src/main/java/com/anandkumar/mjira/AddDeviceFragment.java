f04d3c971a0c1ed51c36058d95c77b09f511db22
karanianandkumar <karanianandkumar@yahoo.com>
2016-06-17 14:34:46 +0530

    32. Add device is updated with TextWatchers

--- app/src/main/java/com/anandkumar/mjira/AddDeviceFragment.java
+++ app/src/main/java/com/anandkumar/mjira/AddDeviceFragment.java
@@ -4,10 +4,14 @@ package com.anandkumar.mjira;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -27,6 +31,8 @@ public class AddDeviceFragment extends Fragment {
     private Button addButton;
     private ProgressDialog progress;
 
+    private TextInputLayout inputLayoutDevice, inputLayoutIMEI;
+
     public AddDeviceFragment() {
         // Required empty public constructor
     }
@@ -37,6 +43,11 @@ public class AddDeviceFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_add_device, container, false);
+
+        inputLayoutDevice = (TextInputLayout) view.findViewById(R.id.input_device_name);
+
+        inputLayoutIMEI = (TextInputLayout) view.findViewById(R.id.input_imei);
+
         dName=(EditText)view.findViewById(R.id.deviceName);
         dIMEI=(EditText)view.findViewById(R.id.deviceIMEI);
         addButton=(Button)view.findViewById(R.id.addDevice);
@@ -45,8 +56,12 @@ public class AddDeviceFragment extends Fragment {
         final String currentUser=intent.getStringExtra("currentUser");
 
 
+        dName.addTextChangedListener(new MyTextWatcher(dName));
+
+        dIMEI.addTextChangedListener(new MyTextWatcher(dIMEI));
+
         progress = new ProgressDialog(getActivity());
-        progress.setTitle("Loading");
+
         progress.setMessage("Wait while Adding Device...");
         progress.setCancelable(false);
 
@@ -69,6 +84,15 @@ public class AddDeviceFragment extends Fragment {
 
 
     private void addNewDetails(Device device) {
+        if (!validateName()) {
+            return;
+        }
+
+
+
+        if (!validateIMEI()) {
+            return;
+        }
         progress.show();
         Backendless.Persistence.save(device, new AsyncCallback<Device>() {
             @Override
@@ -86,5 +110,68 @@ public class AddDeviceFragment extends Fragment {
             }
         });
     }
+
+    private boolean validateName() {
+        if (dName.getText().toString().trim().isEmpty()) {
+            dName.setError(getString(R.string.err_msg_name));
+            requestFocus(dName);
+            return false;
+        } else {
+            inputLayoutDevice.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+
+
+    private boolean validateIMEI() {
+        if (dIMEI.getText().toString().trim().isEmpty()) {
+            dIMEI.setError(getString(R.string.err_msg_imei));
+            requestFocus(dIMEI);
+            return false;
+        } else {
+            inputLayoutIMEI.setErrorEnabled(false);
+        }
+
+        return true;
+
+    }
+
+
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    private class MyTextWatcher implements TextWatcher {
+
+        private View view;
+
+        private MyTextWatcher(View view) {
+            this.view = view;
+        }
+
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        public void afterTextChanged(Editable editable) {
+            switch (view.getId()) {
+                case R.id.deviceName:
+                    validateName();
+                    break;
+
+                case R.id.deviceIMEI:
+                    validateIMEI();
+                    break;
+            }
+        }
+    }
+
 
 }
